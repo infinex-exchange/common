831c550fd56d7305067e86bfc97dd5f2b34a86b7
infinex-exchange <admin@infinex.cc>
2023-09-06 15:56:35 +0200

    Syntax

--- src/AMQP/AMQP.php
+++ src/AMQP/AMQP.php
@@ -6,7 +6,7 @@ use Evenement\EventEmitter;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Exchange\AMQPExchangeType;
 use PhpAmqpLib\Message\AMQPMessage;
-use PhpAmqpLib\Wire;
+use PhpAmqpLib\Wire\AMQPTable;
 use React\Promise\Promise;
 use React\Promise\Deferred;
 
@@ -88,7 +88,7 @@ class AMQP extends EventEmitter {
         $headers['event'] = $event;
         
         $msg = new AMQPMessage(json_encode($body, JSON_PRETTY_PRINT));
-        $msg -> set('application_headers', new \Wire\AMQPTable($headers));
+        $msg -> set('application_headers', new AMQPTable($headers));
         
         $this -> channel -> basic_publish($msg, 'infinex');
     }
@@ -97,7 +97,7 @@ class AMQP extends EventEmitter {
         $headers['event'] = $event;
         
         $this -> channel -> queue_declare($queue, false, false, false, true); // auto delete
-        $this -> channel -> queue_bind($queue, 'infinex', '', false, new \Wire\AMQPTable($headers));
+        $this -> channel -> queue_bind($queue, 'infinex', '', false, new AMQPTable($headers));
         $th = $this;
         $this -> channel -> basic_consume(
             $queue,
