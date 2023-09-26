9df12e270acb28b40b401ee69b01f19d0f006f29
infinex-exchange <admin@infinex.cc>
2023-09-26 13:14:09 +0200

    Offset pagination

---
+++ src/Pagination/Offset.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Infinex\Pagination;
+
+use Infinex\Exceptions\Error;
+
+class Offset {
+    private $offset;
+    private $limit;
+    private $count;
+    public $more;
+    
+    function __construct($maxLimit, $defaultLimit, $userParams) {
+        if(isset($userParams['offset'])) {
+            if(!$this -> validateOffset($userParams['offset']))
+                throw new Error('VALIDATION_ERROR', 'Invalid pagination offset', 400);
+            
+            $this -> offset = $userParams['offset'];
+        } else {
+            $this -> offset = 0;
+        }
+        
+        if(isset($userParams['limit'])) {
+            if(!$this -> validateLimit($userParams['limit']))
+                throw new Error('VALIDATION_ERROR', 'Invalid pagination limit', 400);
+            
+            if($userParams['limit'] > $maxLimit)
+                throw new Error('VALIDATION_ERROR', 'Pagination limit out of range', 400);
+            
+            $this -> limit = $userParams['limit'];
+        } else {
+            $this -> limit = $defaultLimit;
+        }
+        
+        $this -> count = 0;
+        $this -> more = false;
+    }
+    
+    public function iter() {
+        if($this -> count == $this -> limit) {
+            $this -> more = true;
+            return false;
+        }
+        $this -> count++;
+        return true;
+    }
+    
+    public function sql() {
+        return ' LIMIT '.($this -> limit + 1).' OFFSET '.$this -> offset;
+    }
+    
+    private function validateOffset($offset) {
+        if(!is_int($offset)) return false;
+        if($offset < 0) return false;
+        return true;
+    }
+    
+    private function validateLimit($limit) {
+        if(!is_int($limit)) return false;
+        if($limit < 1) return false;
+        return true;
+    }
+}
+
+?>
