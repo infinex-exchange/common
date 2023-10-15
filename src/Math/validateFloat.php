bd8f0148ab85ae88da894fcc9eef767252a02198
infinex-exchange <admin@infinex.cc>
2023-10-15 13:50:48 +0200

    validateFloat negative

--- src/Math/validateFloat.php
+++ src/Math/validateFloat.php
@@ -2,8 +2,10 @@
 
 namespace Infinex\Math;
 
-function validateFloat($float) {
+function validateFloat($float, $allowNegative) {
     if(gettype($float) != 'string') return false;
+    if($allowNegative && @$float[0] == '-')
+        $float = substr($float, 1);
     return preg_match('/^[0-9]{1,33}(\.[0-9]{1,32})?$/', $float);
 }
 
