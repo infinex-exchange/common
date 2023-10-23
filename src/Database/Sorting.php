a17f792a78bf8e44e6749709612ec0493c717d12
infinex-exchange <admin@infinex.cc>
2023-10-23 15:09:31 +0200

    Add database sorting

---
+++ src/Database/Sorting.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Infinex\Database;
+
+use Infinex\Exceptions\Error;
+
+class Sorting {
+    private $orderBy;
+    private $orderDir;
+    
+    function __construct($colMap, $defaultBy, $defaultDir, $userParams) {
+        if(isset($userParams['orderBy'])) {
+            if(!array_key_exists($userParams['orderBy'], $colMap))
+                throw new Error('VALIDATION_ERROR', 'orderBy', 400);
+            
+            $this -> orderBy = $colMap[ $userParams['orderBy'] ];
+        }
+        else
+            $this -> orderBy = $defaultBy;
+        
+        if(isset($userParams['orderDir'])) {
+            if(!in_array($userParsms['orderDir'], ['ASC', 'DESC']))
+                throw new Error('VALIDATION_ERROR', 'orderDir', 400);
+            
+            $this -> orderDir = $userParams['orderDir'];
+        }
+        else
+            $this -> orderDir = $defaultDir;
+    }
+    
+    public function sql() {
+        return ' ORDER BY '.$this -> orderBy.' '.$this -> orderDir;
+    }
+}
+
+?>
