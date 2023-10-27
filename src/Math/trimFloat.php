cab80e931734bb333c88d4581c736ffeced64844
infinex-exchange <admin@infinex.cc>
2023-10-27 17:17:17 +0200

    Fix trimfloat

--- src/Math/trimFloat.php
+++ src/Math/trimFloat.php
@@ -3,10 +3,12 @@
 namespace Infinex\Math;
 
 function trimFloat($float) {
-    if($float === '0' || $float === NULL) return $float;
-    if(strpos($float, '0.') !== 0)
-        $float = ltrim($float, '0');
-    return rtrim(rtrim($float, '0'), '.');
+    $exp = explode('.', $float);
+    $integer = ltrim($exp[0], '0');
+    if($integer == '')
+        $integer = '0';
+    $fractional = isset($exp[1]) ? rtrim($exp[1], '0') : '';
+    return $integer . ($fractional != '' ? '.'.$fractional : '');
 }
 
 ?>
